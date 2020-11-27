25972f01fcf6eda4514be9f684ed5b7c7628ed8f
Federico <fwalovnik@gmail.com>
2020-11-27 11:02:37 -0300

    added a limmit to the walls

--- 05-Random_Walker/sketch.js
+++ 05-Random_Walker/sketch.js
@@ -1,4 +1,4 @@
-//This sketch is inspired by this video: https://www.youtube.com/watch?v=rqecAdEGW6I&list=PLRqwX-V7Uu6YVljJvFRCyRM6mmF5wMPeE&index=2
+//This sketch was inspired by this video: https://www.youtube.com/watch?v=rqecAdEGW6I&list=PLRqwX-V7Uu6YVljJvFRCyRM6mmF5wMPeE&index=2
 
 let Xposition = 0;
 let Yposition = 0;
@@ -12,25 +12,46 @@ function setup() {
 function draw() {
   translate(height / 2, width / 2);
 
-  stroke(255);
+  stroke(random(255));
   strokeWeight(5);
 
   point(Xposition, Yposition);
 
   chooseNextStep();
-  
 }
+
+const checkIfHitsWall = () => {
+  let hitTheWall = false;
+
+  if (Xposition <= -(width - width / 2)) {
+    hitTheWall = true;
+    Xposition += 10;
+  } else if (Xposition >= width + width / 2) {
+    Xposition += 10;
+    hitTheWall = true;
+  } else if (Yposition >= height - height / 2) {
+    Yposition -= 10;
+    hitTheWall = true;
+  } else if (Yposition >= height + height / 2) {
+    Yposition += 10;
+    hitTheWall = true;
+  }
+  return hitTheWall;
+};
 
 const chooseNextStep = () => {
   const randomNumber = Math.round(Math.random() * 3);
 
-  if (randomNumber === 0) {
-    Xposition += 5;
-  } else if (randomNumber === 1) {
-    Yposition += 5;
-  } else if (randomNumber === 2) {
-    Xposition -= 5;
-  } else {
-    Yposition -= 5;
+  console.log(checkIfHitsWall());
+  if (!checkIfHitsWall()) {
+    if (randomNumber === 0) {
+      Xposition += 5;
+    } else if (randomNumber === 1) {
+      Yposition += 5;
+    } else if (randomNumber === 2) {
+      Xposition -= 5;
+    } else {
+      Yposition -= 5;
+    }
   }
 };
