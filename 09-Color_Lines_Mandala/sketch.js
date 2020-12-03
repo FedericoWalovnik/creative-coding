e2d2f987239d5471b83ac0ec6b66fb9ba720eec9
Federico <fwalovnik@gmail.com>
2020-12-02 21:25:19 -0300

    color lines mandala

--- 09-Color_Lines_Mandala/sketch.js
+++ 09-Color_Lines_Mandala/sketch.js
@@ -1,26 +1,27 @@
-let distanceMiddle = 50;
+let distanceMiddle = 5;
 let circleSize = 10;
 let rotationAngle = 0;
-let frames = 8;
+let frames = 10;
 
 function setup() {
   var cnv = createCanvas(600, 600);
   cnv.style("display", "block");
   background(0);
   angleMode(DEGREES);
+  colorMode(HSB);
 }
 
 function draw() {
   frameRate(frames);
-  frames += 0.2;
 
   translate(width / 2, height / 2);
 
-  stroke(255);
+  strokeWeight(random(150));
+  stroke(Math.round(random(255)), 50, 100);
   fill(0, 0, 0, 0);
 
   rotate(rotationAngle);
-  circle(0, distanceMiddle, circleSize);
-  rotationAngle += 20;
-  circleSize += 10;
+  line(0, 0, distanceMiddle, distanceMiddle);
+  rotationAngle += 10;
+  distanceMiddle += 1;
 }
