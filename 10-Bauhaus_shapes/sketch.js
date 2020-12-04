690dc289dd822b5307f56bf75aebab176aac4280
Federico <fwalovnik@gmail.com>
2020-12-04 00:04:48 -0300

    basic random shapes composition done

--- 10-Bauhaus_shapes/sketch.js
+++ 10-Bauhaus_shapes/sketch.js
@@ -17,11 +17,13 @@ function setup() {
   cnv.style("display", "block");
   background(255);
   angleMode(DEGREES);
+
+  noStroke();
 }
 
 function draw() {
-  const randomFigure = Math.round(random(1));
-  
+  const randomFigure = Math.round(random(4));
+
   //get a random color from the array of colors
   const randomColor = random(colors);
   fill(randomColor);
@@ -30,6 +32,33 @@ function draw() {
     arc(Xlocation, Ylocation, sizeOfSquare * 2, sizeOfSquare * 2, 0, 90);
   } else if (randomFigure === 1) {
     square(Xlocation, Ylocation, sizeOfSquare);
+  } else if (randomFigure === 2) {
+    arc(
+      Xlocation + sizeOfSquare,
+      Ylocation,
+      sizeOfSquare * 2,
+      sizeOfSquare * 2,
+      90,
+      180,
+    );
+  } else if (randomFigure === 3) {
+    arc(
+      Xlocation,
+      Ylocation + sizeOfSquare,
+      sizeOfSquare * 2,
+      sizeOfSquare * 2,
+      270,
+      360,
+    );
+  } else if (randomFigure === 4) {
+    arc(
+      Xlocation + sizeOfSquare,
+      Ylocation + sizeOfSquare,
+      sizeOfSquare * 2,
+      sizeOfSquare * 2,
+      180,
+      270,
+    );
   }
 
   Xlocation += sizeOfSquare;
