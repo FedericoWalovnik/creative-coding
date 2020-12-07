70b00560a8d28bf3fdf6dd65b927b5c9a0968fba
Federico <fwalovnik@gmail.com>
2020-12-06 22:37:26 -0300

    explanation of gaussian distribution

---
+++ 11-Gaussian_Distribution_practice/sketch.js
@@ -0,0 +1,14 @@
+//Also known as normal distribution, the Gaussian Distribution is a way of distributing numbers more similar to nature.
+//this distribution of "random" numbers have a mean that its the point of higher chances,
+//when the numbers are further from the mean the probability of being chosen is lower
+
+function setup() {
+  var cnv = createCanvas(600, 600);
+  cnv.style("display", "block");
+  background(255);
+  angleMode(DEGREES);
+
+  noStroke();
+}
+
+function draw() {}
