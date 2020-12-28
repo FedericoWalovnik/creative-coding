b7d7a0e922f19dd4638572f73913261b93a32fe2
FedericoWalovnik <fwalovnik@gmail.com>
2020-12-28 00:35:18 -0300

    average color image finished

--- 26-Image_AverageColor/sketch.js
+++ 26-Image_AverageColor/sketch.js
@@ -6,12 +6,13 @@ let a = 0;
 
 function preload(){
   img = loadImage("sunflowers.jpg");
+  blueskyImg = loadImage("bluesky.jpg");
 }
 
 function setup() {
   angleMode(DEGREES);
-  img.resize(650, 0);
-  var cnv = createCanvas(img.width, img.height);
+  blueskyImg.resize(650, 0);
+  var cnv = createCanvas(blueskyImg.width, blueskyImg.height);
   cnv.style("display", "block");
   background(255);
 
@@ -19,15 +20,17 @@ function setup() {
 
 function draw() {
   loadPixels();
-  image(img, 0, 0);
+  image(blueskyImg, 0, 0);
+  blueskyImg.loadPixels();
 
   for(let y = 0; y < height; y++){
     for(let x = 0; x < width; x++){
       let index = (x + y * width) * 4;
-      r += pixels[index + 0];
-      g += pixels[index + 1];
-      b += pixels[index + 2];
-      a += pixels[index + 3];
+      r += blueskyImg.pixels[index + 0];
+      g += blueskyImg.pixels[index + 1];
+      b += blueskyImg.pixels[index + 2];
+      a += blueskyImg.pixels[index + 3];
+
     }
   }
 
@@ -36,7 +39,11 @@ function draw() {
   b = b / (pixels.length / 4);
   a = a / (pixels.length / 4);
 
-  console.log(r,g,b,a);
+  noStroke();
+  fill(r,g,b);
+  console.log(r,g,b)
+  square(100,100,100);
+
 
 
   //updatePixels();
